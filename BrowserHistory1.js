c1e6ca82465573ca8fbc5dd2ad2adf0f0073da3b
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-06-12 10:34:37 +0300

    Add problem solution

---
+++ BrowserHistory1.js
@@ -0,0 +1,70 @@
+function browserHistory(browserHistoryAsObj, operations) {
+    let openTabs = [];
+    let recentlyClosed = [];
+    let browserLogs = [];
+
+    for (let operation of operations) {
+        openTabs = Object.values(browserHistoryAsObj)[1];
+        recentlyClosed = Object.values(browserHistoryAsObj)[2];
+        browserLogs = Object.values(browserHistoryAsObj)[3];
+        operation = operation.split(' ');
+        let command = operation.shift();
+
+        if (command == 'Open') {
+            let website = operation.shift();
+            openNewTab(openTabs, website, browserLogs, browserHistoryAsObj);
+
+        } else if (command == 'Close') {
+            let website = operation.shift();
+            closeTab(openTabs, recentlyClosed, website, browserLogs, browserHistoryAsObj);
+
+        } else if (command == 'Clear') {
+            clear(openTabs, recentlyClosed, browserLogs, browserHistoryAsObj);
+        }
+    }
+    function openNewTab(openTabs, website, browserLogs, browserHistoryAsObj) {
+        openTabs.push(website);
+        browserLogs.push(`Open ${website}`);
+        browserHistoryAsObj['Open Tabs'] = openTabs;
+        browserHistoryAsObj['Browser Logs'] = browserLogs;
+        return browserHistoryAsObj;
+    }
+
+    function closeTab(openTabs, recentlyClosed, website, browserLogs, browserHistoryAsObj) {
+        let indexOfWebsite = openTabs.indexOf(website);
+        if (indexOfWebsite != -1) {
+            openTabs.splice(indexOfWebsite, 1);
+            recentlyClosed.push(website);
+            browserLogs.push(`Close ${website}`);
+            browserHistoryAsObj['Open Tabs'] = openTabs;
+            browserHistoryAsObj['Recently Closed'] = recentlyClosed;
+            browserHistoryAsObj['Browser Logs'] = browserLogs;
+        }
+        return browserHistoryAsObj;
+    }
+
+    function clear(openTabs, recentlyClosed, browserLogs, browserHistoryAsObj) {
+        openTabs = [];
+        recentlyClosed = [];
+        browserLogs = [];
+        browserHistoryAsObj['Open Tabs'] = openTabs;
+        browserHistoryAsObj['Recently Closed'] = recentlyClosed;
+        browserHistoryAsObj['Browser Logs'] = browserLogs;
+
+        return browserHistoryAsObj;
+    }
+
+    console.log(browserHistoryAsObj['Browser Name']);
+    console.log(`Open Tabs: ${openTabs.join(', ')}`);
+    console.log(`Recently Closed: ${recentlyClosed.join(', ')}`);
+    console.log(`Browser Logs: ${browserLogs.join(', ')}`);
+}
+browserHistory({
+    "Browser Name": "Mozilla Firefox",
+    "Open Tabs": ["YouTube"],
+    "Recently Closed": ["Gmail", "Dropbox"],
+    "Browser Logs": ["Open Gmail", "Close Gmail", "Open Dropbox", "Open YouTube", "Close Dropbox"]
+},
+    ["Open Wikipedia", "Clear History and Cache", "Open Twitter"]
+
+);
